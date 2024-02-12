93868eca8d0a62f68fdf23cff0a28d7ef3fb46a7
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-12 14:46:52 +0000

    WebSocket connection, authentication and subscription

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { JSX, useState } from "react";
+import { ACCESS_TOKEN } from "@env";
+import { JSX, useEffect, useState } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import Animated, { FadeInRight, FadeOutRight } from "react-native-reanimated";
@@ -6,9 +7,67 @@ import Animated, { FadeInRight, FadeOutRight } from "react-native-reanimated";
 import Clock from "@/components/Clock";
 import MainDashboard from "@/components/MainDashboard";
 import Slider from "@/components/Slider";
+import { initialEntities } from "@/data/entities";
+import { EntityMapping } from "@/types/device";
+import { WsState } from "@/types/socket";
 
 export default function App(): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
+  const [wsState, setWsState] = useState<WsState>({
+    connected: false,
+    auth: false,
+    subscribed: false,
+  });
+
+  const [entities, setEntities] = useState<EntityMapping>(initialEntities);
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      "https://home.garridoegarridolda.pt/api/websocket",
+    );
+
+    ws.onopen = () => {
+      setWsState((st) => ({ ...st, connected: true }));
+    };
+
+    ws.onclose = () => {
+      setWsState({ connected: false, auth: false, subscribed: false });
+    };
+
+    ws.onmessage = (e) => {
+      if (!wsState.auth) {
+        ws.send(
+          JSON.stringify({
+            type: "auth",
+            access_token: ACCESS_TOKEN,
+          }),
+        );
+        setWsState((st) => ({ ...st, auth: true }));
+        console.log("Authenticated");
+        return;
+      }
+
+      if (!wsState.subscribed) {
+        const entityIds = Object.entries(entities).map((k) => k[0]);
+
+        ws.send(
+          JSON.stringify({
+            id: 18,
+            type: "subscribe_events",
+            entity_ids: entityIds,
+          }),
+        );
+        setWsState((st) => ({ ...st, subscribed: true }));
+        console.log("Subscribed");
+        return;
+      }
+
+      const data = JSON.parse(e.data);
+      console.log(data);
+    };
+
+    return ws.close();
+  }, []);
 
   return (
     <PaperProvider>
