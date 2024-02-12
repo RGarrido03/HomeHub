73521415a2dbbed365360e8d474bdd4b122704f5
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-12 17:53:06 +0000

    Oops, update event type

--- types/socket.ts
+++ types/socket.ts
@@ -21,35 +21,18 @@ type Result = Message & {
   result: null;
 };
 
-type State = {
-  entity_id: string;
-  last_changed: Date;
-  state: string;
-  attributes?: any;
-  last_updated: Date;
-  context: {
-    id: string;
-    parent_id: string | null;
-    user_id: string;
-  };
-};
-
 export type ReceivedEvent = Message & {
   id: number;
   type: "event";
   event: {
-    data: {
-      entity_id: string;
-      new_state: State;
-      old_state?: State;
-    };
-    event_type: "state_changed";
-    time_fired: Date;
-    origin: string;
-    context: {
-      id: string;
-      parent_id: string | null;
-      user_id: string;
+    c: {
+      [key: string]: {
+        "+": {
+          c: string; // ?
+          lc: number; // Timestamp
+          s: string; // Value
+        };
+      };
     };
   };
 };
