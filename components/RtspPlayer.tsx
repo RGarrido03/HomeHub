89edcfb153cde41c404324b0dd540a66b261ed55
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-01-04 00:17:34 +0000

    Remove vlc media player

--- components/RtspPlayer.tsx
+++ components/RtspPlayer.tsx
@@ -1,16 +1,15 @@
-import { STREAM } from "@env";
-import { StyleSheet } from "react-native";
-import { VLCPlayer } from "react-native-vlc-media-player";
+import { StyleSheet, Text } from "react-native";
 
 export default function RtspPlayer() {
   return (
-    <VLCPlayer
-      style={[styles.video]}
-      source={{
-        uri: STREAM,
-      }}
-      videoAspectRatio="4:3"
-    />
+    <Text style={styles.video}>Not implemented</Text>
+    // <VLCPlayer
+    //   style={[styles.video]}
+    //   source={{
+    //     uri: STREAM,
+    //   }}
+    //   videoAspectRatio="4:3"
+    // />
   );
 }
 
